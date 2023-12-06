9c9175206709a15ef94c7e0119ea183cc721baf7
Andy Kuan <andy952737@gmail.com>
2023-12-06 21:07:39 +0800

    add Passengerfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '139.162.90.195', user: 'deploy', roles: %w{app db web}
+server '192.53.115.148', user: 'deploy', roles: %w{app db web} 
 
 # role-based syntax
 # ==================
